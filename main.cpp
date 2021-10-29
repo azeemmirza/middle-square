ded6df1cac66507dd609c8a0e7273ce6cdb4d399
Azeem Mirza <azeem.mirza94@gmail.com>
2021-10-29 20:40:18 +0500

    feat: main.cpp program initialized

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int seed;
+
+    std::cout << "Middle Square" << std::endl;
+    std::cin >> seed;
+
+    std::cout << "Entered Seed is: " << seed << std::endl;
+    return 0;
+}
