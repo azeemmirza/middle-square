25fc7b3b1f188822ec7e8f79719b9ac02c8b9bd6
Azeem Mirza <azeem.mirza94@gmail.com>
2021-11-01 11:30:21 +0500

    feat: imperative structure of middle-sqaure

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
     int seed;
@@ -7,5 +8,30 @@ int main() {
     std::cin >> seed;
 
     std::cout << "Entered Seed is: " << seed << std::endl;
+
+    int seedLength = std::to_string(seed).length();
+
+    std::cout << seedLength << std::endl;
+
+    if (seedLength < 2) {
+        std::cout << "Seed must at least above 10";
+        return 0;
+    }
+
+    int middle = seed;
+    int square;
+    std::string squareString;
+    int squareLength;
+
+    for (int i = 0; i < 50; i++) {
+        square = middle * middle;
+        squareString = std::to_string(square);
+        squareLength = squareString.length();
+        middle = std::stoi(squareString.substr((squareLength - 1) / 2, 2 - squareLength % 2));
+
+        std::cout << i << " - " << square << " - " << middle << std::endl;
+
+    }
+
     return 0;
 }
